3d5419902f673b20f0650c45c2287b1bfc555679
leanke <theleanke@gmail.com>
2025-02-03 18:54:12 -0600

    Initial commit of dotfiles

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ln -sf ~/dotfiles/.bashrc ~/.bashrc \
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf \
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases \
+ln -sf ~/dotfiles/.bash_completion ~/.bash_completion \
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig \
+ln -sf ~/dotfiles/.profile ~/.profile \
+ln -sf ~/dotfiles/.selected_editor ~/.selected_editor \
+ln -sf ~/dotfiles/.netrc ~/.netrc \
+ln -sf ~/dotfiles/.config/micro/ ~/.config/micro/
